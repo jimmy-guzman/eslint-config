59ece3123d5b2f4f3d17a15fccca8219d3142e50
jimmy-guzman <hi@jimmy.codes>
2025-07-09 17:00:26 -0500

    feat: ✨ enable `@tanstack/query/mutation-property-order`

--- src/configs/tanstack-query.ts
+++ src/configs/tanstack-query.ts
@@ -18,6 +18,7 @@ export default async function tanstackQueryConfig() {
       rules: {
         "@tanstack/query/exhaustive-deps": "error",
         "@tanstack/query/infinite-query-property-order": "error",
+        "@tanstack/query/mutation-property-order": "error",
         "@tanstack/query/no-rest-destructuring": "error",
         "@tanstack/query/no-unstable-deps": "error",
         "@tanstack/query/no-void-query-fn": "error",
