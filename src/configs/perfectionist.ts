4d113822929644718d3efac6bcceb0498bddfd2d
jimmy-guzman <hi@jimmy.codes>
2025-07-21 19:42:11 -0500

    feat: ✨ bump deps to latest minor

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -1,20 +1,17 @@
-import type { ESLint } from "eslint";
-
 import perfectionist from "eslint-plugin-perfectionist";
 
 import type { TypedConfigItem } from "../types";
 
 import { perfectionistRules } from "../rules/perfectionist";
 
-export const perfectionistConfig = () => {
+export const perfectionistConfig = (): TypedConfigItem[] => {
   return [
     {
       name: "jimmy.codes/perfectionist",
       plugins: {
-        // TODO: remove unknown conversion
-        perfectionist: perfectionist as unknown as ESLint.Plugin,
+        perfectionist,
       },
       rules: perfectionistRules,
     },
-  ] satisfies TypedConfigItem[];
+  ];
 };
