f4d729d51a47d06cfc9ee9f85f274f9008eef8ef
jimmy-guzman <hi@jimmy.codes>
2025-11-05 07:33:31 -0600

    fix: 🐛 use `.mjs` extension for better `node` support

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  clean: true,
-  dts: true,
   entry: ["src/index.ts", "src/globs.ts"],
-  format: ["esm"],
   publint: true,
   shims: true,
 });
