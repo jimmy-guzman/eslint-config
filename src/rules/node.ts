1da57249faa7131ccd96f9e03d8ec5514c5f8c20
jimmy-guzman <hi@jimmy.codes>
2026-02-16 13:21:16 -0600

    feat: ✨ enable node prefer global rules

--- src/rules/node.ts
+++ src/rules/node.ts
@@ -8,7 +8,15 @@ export const nodeRules = {
   "n/no-path-concat": "error",
   "n/no-process-exit": "off", // TODO [2026-12-31]: enable this rule
   "n/no-top-level-await": ["error", { ignoreBin: true }],
+  "n/prefer-global/buffer": ["error", "always"],
   "n/prefer-global/console": ["error", "always"],
+  "n/prefer-global/crypto": ["error", "never"],
+  "n/prefer-global/process": ["error", "always"],
+  "n/prefer-global/text-decoder": ["error", "never"],
+  "n/prefer-global/text-encoder": ["error", "never"],
+  "n/prefer-global/timers": ["error", "always"],
+  "n/prefer-global/url": ["error", "always"],
+  "n/prefer-global/url-search-params": ["error", "always"],
   "n/prefer-node-protocol": "error",
   "n/process-exit-as-throw": "error",
 } satisfies Rules;
